a30a0a7b833e737cc658837ecd250ccd08a8087e
Samara <igor_samardzija@yahoo.com>
2018-01-29 21:51:40 +0100

    added ArticleService (Transactional and Validated)

---
+++ cxf-springboot-example/src/main/java/hr/samara/service/ArticleService.java
@@ -0,0 +1,18 @@
+package hr.samara.service;
+
+import hr.samara.model.Article;
+import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import java.util.List;
+
+@Validated
+public interface ArticleService {
+    List<Article> fetchArticles();
+    Article findArticleByName(@NotBlank(message = "{name.blank}") String name);
+    void updateArticlePrice(@Min(value = 1, message = "{id.positive}") Long id, @Min(value = 0, message = "{price.positive}") BigDecimal price);
+    Article saveArticle(@NotNull Article article);
+}
---
+++ cxf-springboot-example/src/main/java/hr/samara/service/ArticleServiceImpl.java
@@ -0,0 +1,52 @@
+package hr.samara.service;
+
+import hr.samara.dao.ArticleRepository;
+import hr.samara.model.Article;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Service
+@Transactional
+public class ArticleServiceImpl implements ArticleService {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final ArticleRepository articleRepository;
+
+    public ArticleServiceImpl(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Article> fetchArticles() {
+        List<Article> articles = this.articleRepository.findAll();
+        logger.info("fetched " + articles.size() + " articles");
+        logger.debug("fetched " + articles);
+        return articles;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Article findArticleByName(String name) {
+        Article article = this.articleRepository.findByName(name);
+        logger.info("fetched " + article);
+        return article;
+    }
+
+    @Override
+    public void updateArticlePrice(Long id, BigDecimal price) {
+        logger.info("update article.id=" + id + " with price=" + price);
+        this.articleRepository.updatePrice(id, price);
+    }
+
+    @Override
+    public Article saveArticle(Article article) {
+        logger.info("save " + article);
+        return this.articleRepository.save(article);
+    }
+}
